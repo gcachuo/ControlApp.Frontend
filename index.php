e84d9c0af98ede7d40bfbc7c2e03efa653ba1d1b
Memo Cachú <gcachu.o@gmail.com>
2024-09-20 03:46:24 -0600

    Refactor

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use Twig\Loader\FilesystemLoader;
 
 class Program
 {
+    private string $title = "Control App";
     private Environment $twig;
 
     private function Init(): void
@@ -30,13 +31,13 @@ class Program
 
         $request_uri = trim($_SERVER['REQUEST_URI'], '/');
 
-        $view_path = __DIR__ . '/views/' . $request_uri . '.php';
+        $view_path = __DIR__ . "/views/$request_uri.php";
         if (!file_exists($view_path)) {
             $request_uri .= '/index';
         }
 
         try {
-            echo $this->twig->render('index.twig', ['title' => 'Control App', 'view' => $request_uri]);
+            echo $this->twig->render('index.twig', ['title' => $this->title, 'view' => $request_uri]);
         } catch (LoaderError $e) {
             http_response_code(404);
             echo $this->twig->render('error.twig', ['message' => $e->getMessage(), 'code' => 404]);
