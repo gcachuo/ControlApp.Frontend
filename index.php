71f1aaacc7a2f8101e282bb859bcf255b64d62bc
Memo Cachú <gcachu.o@gmail.com>
2024-09-20 03:35:23 -0600

    [CAF-16] Add Program and Main

--- index.php
+++ index.php
@@ -4,22 +4,43 @@ use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Loader\FilesystemLoader;
 
-require_once __DIR__.'/vendor/autoload.php';
+(new Program())->Main();
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+class Program
+{
+    private Environment $twig;
 
-$loader = new FilesystemLoader([__DIR__.'/templates/',__DIR__.'/views/']);
-$twig = new Environment($loader, [
-    'cache' => __DIR__.'/cache/',
-    'debug' => true
-]);
+    private function Init(): void
+    {
+        require_once __DIR__ . '/vendor/autoload.php';
 
-$request_uri = trim($_SERVER['REQUEST_URI'], '/');
+        error_reporting(E_ALL);
+        ini_set('display_errors', '1');
 
-try{
-echo $twig->render('index.twig', ['title' => 'Control App', 'view' => $request_uri]);
-}catch(LoaderError $e){
-    http_response_code(404);
-    echo $twig->render('error.twig', ['message' => $e->getMessage(), 'code'=>404]);
+        $loader = new FilesystemLoader([__DIR__ . '/templates/', __DIR__ . '/views/']);
+        $this->twig = new Environment($loader, [
+            'cache' => __DIR__ . '/cache/',
+            'debug' => true
+        ]);
+    }
+
+    public function Main(): void
+    {
+        $this->Init();
+
+        $request_uri = trim($_SERVER['REQUEST_URI'], '/');
+
+        $view_path = __DIR__ . '/views/' . $request_uri . '.php';
+        if (!file_exists($view_path)) {
+            $request_uri .= '/index';
+        }
+
+        try {
+            echo $this->twig->render('index.twig', ['title' => 'Control App', 'view' => $request_uri]);
+        } catch (LoaderError $e) {
+            http_response_code(404);
+            echo $this->twig->render('error.twig', ['message' => $e->getMessage(), 'code' => 404]);
+        }
+    }
 }
+
