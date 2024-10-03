471532d4630e4e074a5f3a483765fdc5648264a4
IvanJGG <ivangallardoguevara@gmail.com>
2024-10-03 17:30:51 -0600

    Create fetch async

---
+++ actions/jwt-decode.php
@@ -0,0 +1,3 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+exit(json_encode(compact( '_POST')));
--- tests/cypress/e2e/dashboard.cy.js
+++ tests/cypress/e2e/dashboard.cy.js
@@ -1,7 +1,7 @@
 describe('Role-based button functionality', () => {
 
     it('should display and interact with admin buttons', () => {
-        cy.visit('http://localhost/dashbord/index?disable-twig-cache=true');
+        cy.visit('http://localhost/dashboard?disable-twig-cache=true');
 
         cy.window().then((window) => {
             const adminToken = 'admin_access_token';
@@ -14,16 +14,16 @@ describe('Role-based button functionality', () => {
         cy.get('#guard').should('not.be.visible');
         cy.get('#forbidden').should('not.be.visible');
 
-        cy.get('button').contains('Usuarios').click();
+        cy.get('#btnUsers').contains('Usuarios').click();
         cy.url().should('include', '/users');
 
-        cy.visit('http://localhost/dashboard/index?disable-twig-cache=true');
-        cy.get('button').contains('Roles y permisos').click();
+        cy.visit('http://localhost/dashboard?disable-twig-cache=true');
+        cy.get('#btnRoles').contains('Roles y permisos').click();
         cy.url().should('include', '/roles');
     });
 
     it('should display and interact with guard buttons', () => {
-        cy.visit('http://localhost/dashboard/index?disable-twig-cache=true');
+        cy.visit('http://localhost/dashboard?disable-twig-cache=true');
 
         cy.window().then((window) => {
             const guardToken = 'guard_access_token';
@@ -36,16 +36,16 @@ describe('Role-based button functionality', () => {
         cy.get('#admin').should('not.be.visible');
         cy.get('#forbidden').should('not.be.visible');
 
-        cy.get('button').contains('Control de visitas').click();
+        cy.get('#btnVisits').contains('Control de visitas').click();
         cy.url().should('include', '/visits');
         
-        cy.visit('http://localhost/dashboard/index?disable-twig-cache=true');
-        cy.get('button').contains('Control de paquetería').click();
+        cy.visit('http://localhost/dashboard?disable-twig-cache=true');
+        cy.get('#btnParcelControl').contains('Control de paquetería').click();
         cy.url().should('include', '/parcelControl');
     });
 
     it('should display forbidden error for unauthorized role', () => {
-        cy.visit('http://localhost/dshboard/index?disable-twig-cache=true');
+        cy.visit('http://localhost/dashboard?disable-twig-cache=true');
 
         cy.window().then((window) => {
             const userToken = 'user_access_token';
