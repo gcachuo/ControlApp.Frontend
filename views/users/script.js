a274669e9aa7ee8e66b685e93baa4c13a62b6640
IvanJGG <ivangallardoguevara@gmail.com>
2024-10-08 19:10:53 -0600

    [FEATURE]CAF-24 Connect editor endpoint (#7)
    * Add endpoint of edit user on handleSubmit
    
    * [FEATURE]CAF-24 Create update testing
    
    * [FEATURE]CAF-24 Fix test users
    
    * [FEATURE]CAF-24 Editing Interface
    
    * [FEATURE]CAF-24 Delete Passsword Field from EditUser
    
    * [FEATURE]CAF-24 Fix Test
    
    * [FEATURE]CAF-24 Doing Test
    
    * CAF-24 Fix Test and Complement task
    
    * CAF-24 Delete update file and add reset form
    
    * CAF-24 Resolve commits

--- views/users/script.js
+++ views/users/script.js
@@ -1,51 +1,111 @@
 window.addEventListener("load", async (event) => {
-    let id=document.getElementById("txtId").value;
-    if(id){
+    let id = document.getElementById("txtId").value;
+    const titulo = document.getElementById('lblTitulo');
+    const passwordField = document.getElementById('passwordField');
+    if (id) {
+        passwordField.remove();
+        titulo.textContent = 'Edicion de Usuario'
         const result = await fetch(`http://localhost:5033/users/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             },
         });
-        let response=await result.json();
-        let user=response.user;
-        document.getElementById("txtEmail").value=user.email;
-        document.getElementById("txtFirstName").value=user.firstName;
-        document.getElementById("txtSecondName").value=user.secondName;
-        document.getElementById("txtFirstSurname").value=user.lastname;
-        document.getElementById("txtSecondSurname").value=user.secondLastname;
-        document.getElementById("txtPhone").value=user.phoneNumber;
-        document.getElementById("txtAddress").value=user.address;
+        let response = await result.json();
+        let user = response.user;
+        document.getElementById("txtEmail").value = user.email;
+        document.getElementById("txtFirstName").value = user.firstName;
+        document.getElementById("txtSecondName").value = user.secondName || null;
+        document.getElementById("txtFirstSurname").value = user.lastname;
+        document.getElementById("txtSecondSurname").value = user.secondLastname || null;
+        document.getElementById("txtPhone").value = user.phoneNumber;
+        document.getElementById("txtAddress").value = user.address;
     }
 });
-async function handleSubmit(e, form){
+
+async function editUser(id, userData) {
+    let uri = id;
+    let method = 'PATCH';
+    let jsonData = userData;
+
+    try {
+        let response = await requestUser(uri, method, jsonData);
+        const result = await response.json();
+
+        if (response.ok) {
+            alert("Usuario actualizado correctamente");
+            window.location.href = '/users/';
+        } else {
+            alert('Error: ' + result.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Hubo un problema al modificar el usuario.');
+    }
+}
+
+async function createUser(userData) {
+    let uri = 'register';
+    let method = 'POST';
+    let jsonData = userData;
+    const form = document.getElementById('userForm');
+
+    try {
+        let response = await requestUser(uri, method, jsonData);
+        const result = await response.json();
+
+        if (response.ok) {
+            alert("El usuario se ha creado con éxito");
+            form.reset();
+        } else {
+            alert('Error: ' + result.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Hubo un problema al crear el usuario.');
+    }
+}
+
+async function requestUser(uri, method, jsonData) {
+    const baseUrl = 'http://localhost:5033';
+    const usersUrl = 'users';
+
+    let url = `${baseUrl}/${usersUrl}/${uri}`;
+
+    const response = await fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonData)
+    });
+
+    if (!response.ok) {
+        const errorResponse = await response.json();
+        throw new Error(errorResponse.message || 'Error al procesar la solicitud');
+    }
+    return response;
+}
+
+async function handleSubmit(e, form) {
     e.preventDefault();
 
     const formData = new FormData(form);
-
     let jsonData = {};
 
-    formData.forEach(function(value, key){
+    formData.forEach(function (value, key) {
         jsonData[key] = value;
     });
 
     try {
-      
-        const response = await fetch('http://localhost:5033/users/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(jsonData)
-        });
+        let id = document.getElementById("txtId").value;
 
-        const result = await response.json();
-
-        if (response.ok) {
-            alert('Usuario registrado con éxito');
-            location.reload();
+        if (id) {
+            // SI EXISTE EL ID ENTONCES ES EDICION 
+            await editUser(id, jsonData); // Usar await aquí
         } else {
-            alert('Error: ' + result.message);
+            // SI NO EXISTE EL ID ENTONCES ES CREACION
+            await createUser(jsonData); // Usar await aquí
         }
     } catch (error) {
         console.error('Error:', error);
