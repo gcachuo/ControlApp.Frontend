94b80c0fe41bad340ed39aa17ef62e3855c0a1a7
IvanJGG <ivangallardoguevara@gmail.com>
2024-10-22 12:17:07 -0600

    [FEATURE]CAF-11 User List Interface (#12)
    * Add dashboard
    
    * CAF-11 Create User List Interface and Testing
    
    * CAF-11 Add icon in Script
    
    * CAF-11 Edit button
    
    * CAF-11 Add location to edit
    
    * CAF-11 Create User List Interface and Testing
    
    * CAF-11 Add icon in Script
    
    * CAF-11 Edit button
    
    * CAF-11 Add location to edit
    
    * CAF-11 WIP test
    
    * CAF-11: Creation of add, index, and edit test files.
    
    * CAF-11 Resolve commits
    
    * CAF-11 Complete should have the correct values in the dropdown
    
    * CAF-11 Resolve commits
    
    * CAF-11 Resolve commits
    
    * CAF-11 Resolve commits
    
    * CAF-11 Create more functions
    
    * CAF-11 Create more functions
    
    ---------
    
    Co-authored-by: Guillermo Cachú Osorio <gcachu.o@gmail.com>

---
+++ tests/cypress/e2e/users.index.cy.js
@@ -0,0 +1,54 @@
+describe('User Table Tests', () => {
+    beforeEach(() => {
+        cy.intercept('GET', '/users', {
+            statusCode: 200,
+            body: {
+                message: 'OK',
+                addresses: [
+                    {
+                        id: 1,
+                        address: 'Calle 1',
+                        firstName: 'Juan',
+                        lastName: 'Pérez',
+                        phoneNumber: '4771234567'
+                    },
+                    {
+                        id: 2,
+                        address: 'Calle 2',
+                        firstName: 'María',
+                        lastName: 'Fernanda',
+                        phoneNumber: '4771234567'
+                    },
+                    {
+                        id: 3,
+                        address: 'Calle 3',
+                        firstName: 'Pedro',
+                        lastName: 'Moreno',
+                        phoneNumber: '4771234567'
+                    }
+                ]
+            },
+        }).as('getUsers');
+
+    });
+
+    it('redirects to the add user page when edit buttons are clicked', () => {
+        cy.visit('http://localhost/users/index/?disable-twig-cache=true');
+
+        cy.wait('@getUsers');
+
+
+        cy.get('[data-cy="btnEdit"]').should('exist');
+
+        cy.get('#userTable tr').then(($rows) => {
+            const numRows = $rows.length;
+            cy.get('[data-cy="btnEdit"]').should('have.length', numRows);
+        });
+
+        cy.get('button[data-cy="btnDeactivate"]').each((button) => {
+            cy.wrap(button).should('be.disabled');
+        });
+
+    });
+
+});
