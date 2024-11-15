b7614a6e7f0ecc96d9a0e49ea95c9920045a2d1c
Memo Cachú <gcachu.o@gmail.com>
2024-11-15 17:46:22 -0600

    fix lint

--- views/roles/assets/index.js
+++ views/roles/assets/index.js
@@ -62,7 +62,6 @@ async function loadRolesTable() {
 
     if (result && result.ok) {
       const role = await result.json();
-      console.log(role);
       const roleTable = document.getElementById("roleTable");
       roleTable.innerHTML = "";
 
