92b1e8abd6a9c3c8bd2d62bc4aeee3c3ec4cf0c7
IvanJGG <ivangallardoguevara@gmail.com>
2024-11-15 17:48:51 -0600

    [FEATURE]CAF-39 Create Roles Table (#15)
    * CAF-37 Fix envVars variable
    
    * CAF-39 Create rows for table
    
    * CAF-39 Create test
    
    * CAF-37 Refactor to axios
    
    * CAF-37 Refactor to axios
    
    * CAF-37 Refactor to axios
    
    * CAF-37 Fix addresses
    
    * Fixes
    
    * load files
    
    * Move assets
    
    * Change assets url
    
    * CAF-39 Correct roleCell
    
    * CAF-39 Correct Test
    
    * Add lint
    
    * Add lint
    
    * Add lint
    
    * Add prettier
    
    * Add prettier
    
    * Add prettier
    
    * Add prettier
    
    * check prettier
    
    * format prettier
    
    * fix lint
    
    ---------
    
    Co-authored-by: Memo Cachú <gcachu.o@gmail.com>

--- views/dashboard/script.js
+++ views/dashboard/script.js
@@ -1,38 +1,35 @@
 window.addEventListener("load", async (event) => {
-    // Obtener el token desde localStorage
-    const token = localStorage.getItem('access_token');
+  // Obtener el token desde localStorage
+  const token = localStorage.getItem("access_token");
 
-    // Decodificar el token para obtener los datos
-    const decodedToken = await jwtDecode(token);
+  // Decodificar el token para obtener los datos
+  const decodedToken = await jwtDecode(token);
 
-    // Obtener el rol del usuario
-    const role = decodedToken.role;  // Asegúrate de que el token contenga la información de rol
+  // Obtener el rol del usuario
+  const role = decodedToken.role; // Asegúrate de que el token contenga la información de rol
 
-    showButtonsByRole(role)
-
+  showButtonsByRole(role);
 });
 
 function navigateTo(page) {
-    window.location.href = page;
+  window.location.href = page;
 }
 
 async function jwtDecode(token) {
-    const response = await actionRequest('POST', 'jwt-decode', {token});
-    return response.data;
+  const response = await actionRequest("POST", "jwt-decode", { token });
+  return response.data;
 }
 
 // Función para mostrar los botones basados en el rol
 function showButtonsByRole(role) {
-    if (role === 'admin') {
-        // Mostrar los tres botones si es administrador
-        document.getElementById('admin').style.display = 'block';
-    } else if (role === 'guard') {
-        // Mostrar solo el botón de guardia
-        document.getElementById('guard').style.display = 'block';
-
-    } else {
-        //Envia a error 403
-        document.getElementById('forbidden').style.display = 'block';
-
-    }
+  if (role === "admin") {
+    // Mostrar los tres botones si es administrador
+    document.getElementById("admin").style.display = "block";
+  } else if (role === "guard") {
+    // Mostrar solo el botón de guardia
+    document.getElementById("guard").style.display = "block";
+  } else {
+    //Envia a error 403
+    document.getElementById("forbidden").style.display = "block";
+  }
 }
